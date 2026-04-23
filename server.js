256982ac849add4718ed76f248e6a604f5b92460
d4v-dev <d4v.mendes086@gmail.com>
2026-04-23 01:39:18 -0300

    Corrigindo o Backend

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get("/api/skills", (req, res) => {
   ]);
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Backend: http://localhost:3000/api/skills");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("🚀 Backend rodando na porta ${PORT}");
 });
